05c886e060206e3c6c7e79fccbb78ee88acf4fd6
Derek Truong <derektruongdev@gmail.com>
2023-04-03 21:11:07 -0500

    initial layout, constants, and state variables

---
+++ script.js
@@ -0,0 +1,74 @@
+/*----- constants -----*/
+const wordBank = [
+  "asteroid",
+  "comet",
+  "nebula",
+  "orbit",
+  "planet",
+  "meteor",
+  "star",
+  "moon",
+  "galaxy",
+  "telescope",
+  "cosmic",
+  "cosmos",
+  "gravity",
+  "universe",
+  "alien",
+  "eclipse",
+  "horizon",
+  "exploration",
+  "interstellar",
+  "constellation"
+];
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+/*----- state variables -----*/
+let secretWord;
+let playerCurrentGuess;
+let numberOfGuesses;
+
+
+/*----- cached elements  -----*/
+const letterDivs = document.querySelectorAll('#alphabet div');
+const secretWordContainer = document.getElementById('secretWordContainer');
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+initialize();
+
+function initialize() {
+  secretWord = chooseSecretWord();
+  playerCurrentGuess = '';
+  numberOfGuesses = 0;
+}
+
+function chooseSecretWord() {
+  return wordBank[Math.floor(Math.random() * wordBank.length)];
+}
+
+function render() {
+  createAlphabet();
+  createSecredWordDiv()
+}
+
+function createAlphabet() {
+  letterDivs.forEach((div, idx) => { 
+    div.innerText = alphabet[idx]
+    if (alphabet[idx] === '') {
+      div.style.backgroundColor = 'red'
+    }
+  });
+}
+
+function createSecredWordDiv() {
+  let len = secretWord.length;
+  
+  while(len > 0) {
+    const newDiv = document.createElement("div")
+    secretWordContainer.appendChild(newDiv);
+    len --;
+  }
+}
